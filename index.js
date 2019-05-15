1d312dd483669020e34f64ec036eab8a44320d31
jennydang <jenny.dang117@gmail.com>
2019-05-15 14:43:41 +0200

    markup

--- index.js
+++ index.js
@@ -2,8 +2,13 @@
  * @format
  */
 
+// Import library
+//import React from 'react'
+//import ReactNative from 'react-native'
+
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+//Render
 AppRegistry.registerComponent(appName, () => App);
