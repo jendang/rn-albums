e816c5023df9cc22e38e2aeb8a25142b0f557912
jennydang <jenny.dang117@gmail.com>
2019-05-15 15:52:29 +0200

    fixed bug

--- App.js
+++ App.js
@@ -1,35 +1,17 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
 import {
- // Platform, 
   StyleSheet, 
   Text, 
   View
 } from 'react-native';
 
-/* const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-}); */
-
-//type Props = {};
+import Header from './src/components/Header'
 
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Hello Papa thoi!</Text>
-        <Text style={styles.instructions}>Hello Hope thoi</Text>
-        {/* <Text style={styles.instructions}>{instructions}</Text> */}
+      <View>
+        <Header headerText={`Albums!`} />
       </View>
     );
   }
@@ -42,14 +24,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+
 });
